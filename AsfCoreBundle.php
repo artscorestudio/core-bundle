4c077aca5a172431d6cddd0bfa365d437f984179
Nicolas Claverie <info@artscore-studio.fr>
2016-01-15 12:19:26 +0100

    Add AsfCoreBundle class

---
+++ AsfCoreBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Artscore Studio Framework package.
+ *
+ * Copyright (c) 2012-2015 Nicolas Claverie <info@artscore-studio.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Asf\Bundle\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Core Bundle
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class AsfCoreBundle extends Bundle {}
