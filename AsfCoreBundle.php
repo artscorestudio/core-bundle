bf26ffc133b71dc70837f18563a962b67d099923
Nicolas Claverie <info@artscore-studio.fr>
2016-01-15 12:33:46 +0100

    Fix typo structure

--- AsfCoreBundle.php
+++ AsfCoreBundle.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Artscore Studio Framework package.
  *
- * Copyright (c) 2012-2015 Nicolas Claverie <info@artscore-studio.fr>
+ * (c) 2012-2015 Nicolas Claverie <info@artscore-studio.fr>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
