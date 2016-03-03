477e9ab9aa4592b990752c60c8e1ab0f1f203b75
Nicolas Claverie <info@artscore-studio.fr>
2016-03-03 14:43:51 +0100

    Add @covers annotations

--- Tests/DependencyInjection/Compiler/ASFEntityManagerPassTest.php
+++ Tests/DependencyInjection/Compiler/ASFEntityManagerPassTest.php
@@ -9,10 +9,9 @@
  */
 namespace ASF\CoreBundle\DependencyInjection\Compiler;
 
-use \Mockery as m;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-
 use ASF\CoreBundle\DependencyInjection\Compiler\ASFEntityManagerPass;
+use \Mockery as m;
 
 /**
  * Compiler Pass for Entity Managers tagged services
@@ -23,7 +22,7 @@ use ASF\CoreBundle\DependencyInjection\Compiler\ASFEntityManagerPass;
 class ASFEntityManagerPassTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Test compiler pass without any services are defined as entity manager
+     * @covers ASF\CoreBundle\DependencyInjection\Compiler\ASFEntityManagerPass
      */
     public function testProcessWithoutDefinedServices()
     {
@@ -34,21 +33,21 @@ class ASFEntityManagerPassTest extends \PHPUnit_Framework_TestCase
     }
     
     /**
-     * Test compiler pass with service defined as entity manager but no custom Entity Manager and with FQCN entity class name
+     * @covers ASF\CoreBundle\DependencyInjection\Compiler\ASFEntityManagerPass
      */
     public function testProcessWithoutManagerClassAndWithFQCNEntity()
     {
     	$manager = m::mock('ASF\CoreBundle\Entity\Manager\ASFEntityManager');
     	$container = new ContainerBuilder();
-    	$container->register('foo.manager', $manager)->addTag('asf_core.manager', array('entity' => 'ASF\CoreBundle\Tests\Fixtures\Manager\MockUser'));
-    	 
+    	$container->register('foo.manager', $manager)->addTag('asf_core.manager', array('entity' => 'ASF\CoreBundle\Entity\MockUser'));
+    	
     	$compiler = new ASFEntityManagerPass();
     	$compiler->process($container);
     	$this->assertTrue($container->hasDefinition('foo.manager'));
     }
     
     /**
-     * Test compiler pass with service defined as entity manager but no custom Entity Manager and with short entity class name
+     * @covers ASF\CoreBundle\DependencyInjection\Compiler\ASFEntityManagerPass
      */
     public function testProcessWithoutManagerClassAndWithShortEntity()
     {
